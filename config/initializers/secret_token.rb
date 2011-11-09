773a2e7b1b4c0e81240015e1a76ab898d24ce50b
Stevan Litobac <stevan@litobac.com>
2011-11-09 10:53:07 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chanarally::Application.config.secret_token = '861ab22b7c875811ca89a0f64f08e737617a61615b5b3250fcf5cd580890bdba0c0c0f0c173d4568fee55ccf6e4890c67eac2648b121fc64db93ef764c7dcad6'
