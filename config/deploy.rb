aea4f0c030c033407366a1acdb11b00bfa423c53
Stevan Litobac <stevan@litobac.com>
2011-11-22 10:49:50 +0000

    Deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
-set :application, "ttlunch2012"
+set :application, "chanarally2012"
 set :user, "deploy"
-set :domain, "ttlunch2012.com"
-set :repository,  "git@github.com:stevanl/ttl.git"
+set :domain, "chanarally2012.com"
+set :repository,  "git@github.com:stevanl/jcr.git"
 set :repository_cache, "git_master"
 set :use_sudo, false
 set :deploy_via, :remote_cache
@@ -23,9 +23,9 @@ task :l do
   set :deploy_to, "/home/deploy/#{application}/live"
 end
 
-role :app, "ttlunch2012.com"
-role :web, "ttlunch2012.com"
-role :db,  "ttlunch2012.com", :primary => true
+role :app, "chanarally2012.com"
+role :web, "chanarally2012.com"
+role :db,  "chanarally2012.com", :primary => true
 
 after "deploy:update_code", "deploy:copy_config_files"
 
