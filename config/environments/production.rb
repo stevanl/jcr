316f293607eaa148ba06fd68e28ad05e6b48d6d6
Stevan Litobac <stevan@litobac.com>
2011-11-24 13:15:43 +0000

    Minor clean up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,8 +61,6 @@ Ttlunch2012::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-
-  
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
@@ -73,9 +71,6 @@ Ttlunch2012::Application.configure do
     :authentication       => 'plain',
     :enable_starttls_auto => true
   }
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
