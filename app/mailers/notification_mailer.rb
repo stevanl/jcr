e64c0ffcc61419b4daf7b25bfdbfc856a5118e29
Stevan Litobac <stevan@litobac.com>
2014-12-23 17:04:09 +0000

    [MAILER] Update mailer subjects

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,10 +2,10 @@ class NotificationMailer < ActionMailer::Base
   default from: "Stevan Litobac <stevan@independentfx.com>"
   def brochure_email(person)
     @person = person
-    mail(:to => ['mark@jam-events.com', 'desi@jam-events.com'], :bcc => 'stevan@mycurrencytransfer.com', :subject => "CR'12: New Brochure Order Form")
+    mail(:to => ['mark@jam-events.com', 'desi@jam-events.com'], :bcc => 'stevan@mycurrencytransfer.com', :subject => "CR'15: New Brochure Order Form")
   end
   def registration_email(person)
     @person = person
-    mail(:to => ['mark@jam-events.com', 'desi@jam-events.com'], :bcc => 'stevan@mycurrencytransfer.com', :subject => "CR'12: New Registration Submitted")
+    mail(:to => ['mark@jam-events.com', 'desi@jam-events.com'], :bcc => 'stevan@mycurrencytransfer.com', :subject => "CR'15: New Registration Submitted")
   end
 end
